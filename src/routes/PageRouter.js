0f5acbce243cecc1b490f278d438186e3116dff2
Hattori-Takuma <hattori.ta@icloud.com>
2022-02-06 14:28:21 +0900

    takuma

--- src/routes/PageRouter.js
+++ src/routes/PageRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from '../App';
 import BtLogin from '../pages/BtLogin';
 import TkLogin from '../pages/TkLogin';
+import TkChat from '../pages/TkChat';
 const PageRouter = () => {
   return (
     <Router>
@@ -10,6 +11,7 @@ const PageRouter = () => {
         <Route path="*" element={<App />} />
         <Route path="/btlogin" element={<BtLogin />} />
         <Route path="/tklogin" element={<TkLogin />} />
+        <Route path="/tkchat/:name" element={<TkChat />} />
       </Routes>
     </Router>
   );
