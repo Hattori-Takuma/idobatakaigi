69318f68c7067a0cc54c12a4c3470508f59e5fa0
tomoya0417 <batayan0281@gmail.com>
2022-02-19 15:13:58 +0900

    ログイン、チャット作成しました

--- src/pages/BtChat.js
+++ src/pages/BtChat.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
+//import TextField from '@mui/material/TextField';
 
 const BtChat = () => {
+
+  const { name } = useParams()
+
   return (
-    <div>
-      <h1>ばたやん用チャットページ</h1>
+  <div>
+    <div className="TkChat-wrapper">
+
+      <h1>チャットページ(tomoya)</h1>
+      <h2>名前・{name}</h2>
+
     </div>
+  </div>
   );
 };
 
