2cb1ac62376c22e1618ec58b49e8b4414e46b6ff
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-03-05 10:40:29 +0900

    modal install

---
+++ src/components/UbModal.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Gravatar from 'react-gravatar'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function UbModal({name, isOpen, isOpenModal}) {
+  console.log(isOpen.toString(), 'is open check')
+  return (
+    <div>
+      <Modal
+        open={isOpen}
+        onClose={isOpenModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <Gravatar
+              email={name}
+              style={{ borderRadius: "25px" }}
+              size={40}
+              default="wavatar"
+              className="CustomAvatar-image"
+              protocol="https://"
+            />
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {name} さん
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
